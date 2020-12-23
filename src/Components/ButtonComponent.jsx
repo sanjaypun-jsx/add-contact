5a3cfbb770658e25eb2db9075fcea768a0047c56
sanjinsky <iamsanjipun@gmail.com>
2020-12-23 18:37:35 +0545

    added fav and edit in contact list

--- src/Components/ButtonComponent.jsx
+++ src/Components/ButtonComponent.jsx
@@ -1,23 +1,23 @@
-import { Button } from "antd";
-import React from "react";
-import styled from "styled-components";
+import { Button } from 'antd';
+import React from 'react';
+import styled from 'styled-components';
 
 const StyledButton = styled(Button)`
   font-size: 1rem;
   height: 2rem;
-  margin: 0rem 0.3rem;
-  background: ${({ bgcolor }) => (bgcolor ? bgcolor : "danger")};
+  margin: 0rem 1rem;
+  background: ${({ bgcolor }) => (bgcolor ? bgcolor : 'danger')};
   color: white;
   outline: none;
   &:hover {
-    background: ${({ bgcolor }) => (bgcolor ? "white" : null)};
+    background: ${({ bgcolor }) => (bgcolor ? 'white' : null)};
   }
 `;
 const ButtonComponent = ({ onClick, bgcolor, type, children }) => {
-  return (
-    <StyledButton onClick={onClick} type={type} bgcolor={bgcolor}>
-      {children}
-    </StyledButton>
-  );
+	return (
+		<StyledButton onClick={onClick} type={type} bgcolor={bgcolor}>
+			{children}
+		</StyledButton>
+	);
 };
 export default ButtonComponent;
--- src/Components/HomeScreen.jsx
+++ src/Components/HomeScreen.jsx
@@ -1,50 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from 'react';
 //import ButtonComponent from "./ButtonComponent";
-import "../styles.css";
-import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
-import styled from "styled-components";
+import '../styles.css';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import styled from 'styled-components';
 //import Modal from "antd/lib/modal/Modal";
 //import { Input } from "antd";
 //import { Alert } from "antd";
-import { useDispatch, useSelector } from "react-redux";
-import { contactListAction } from "../Actions/contactListAction";
+import { useDispatch, useSelector } from 'react-redux';
+import { contactListAction } from '../Actions/contactListAction';
 //import { addContactAction } from "../Actions/addContactAction";
-import Loading from "./Loading";
-import { Alert } from "antd";
+import Loading from './Loading';
+import { Alert } from 'antd';
+import { addToFavouriteAction } from '../Actions/addToFavouriteAction';
+import { contactDetailsAction } from '../Actions/contactDetailsAction';
 
 const Icon = styled.i`
-  color: #white;
-  font-size: 1rem;
-  padding: 0rem 0.5rem;
-  cursor: pointer;
-  &:hover {
-    color: #e7305b;
-  }
+	color: white;
+	font-size: 1rem;
+	padding: 0rem 0.5rem;
+	cursor: pointer;
 `;
 const DIV = styled.div`
-  display: block;
-  height: 3rem;
-  margin-top: 2rem;
+	display: block;
+	height: 3rem;
+	margin-top: 2rem;
 `;
-const DIVWrapper = styled.div`
-  display: block;
-`;
+const DIVWrapper = styled.div`display: block;`;
 const PlaceHolder = styled.div`
-  padding: 0.3rem 1rem;
-  background-color: #e41f7b;
-  font-size: 1rem;
-  margin: 0.2rem;
-  color: white;
-  width: 20rem;
-  height: 3rem;
-  display: flex;
-  align-items: center;
-  text-align: left;
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
-  &:hover {
-    transform: scale(1.05);
-  }
+	padding: 0.3rem 1rem;
+	background-color: #e41f7b;
+	font-size: 1rem;
+	margin: 0.2rem;
+	color: white;
+	width: 20rem;
+	height: 3rem;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	text-align: left;
+	cursor: pointer;
+	transition: all 0.2s ease-in-out;
 `;
 // const AddIcon = styled.i`
 //   color: #ff847c;
@@ -58,61 +53,91 @@ const PlaceHolder = styled.div`
 // `;
 
 export default function HomeScreen({ location, history }) {
-  const dispatch = useDispatch();
+	const [ fav, setFav ] = useState(false);
+	const [ ID, setID ] = useState();
 
-  const contactList = useSelector((state) => state.contactList);
-  const { loading, error, contacts } = contactList;
+	const dispatch = useDispatch();
 
-  const addContact = useSelector((state) => state.addContact);
-  const { contact } = addContact;
+	const contactList = useSelector((state) => state.contactList);
+	const { loading, error, contacts } = contactList;
 
-  useEffect(() => {
-    dispatch(contactListAction());
-  }, [dispatch, contact]);
+	const addContact = useSelector((state) => state.addContact);
+	const { contact: adContact } = addContact;
 
-  const handleDetails = (id) => {
-    history.push(`/details/${id}`);
-  };
+	const editContact = useSelector((state) => state.editContact);
+	const { edited } = editContact;
 
-  return (
-    <div className="App">
-      <DIVWrapper>
-        {error ? <Alert type="error" message="Something went wrong!" /> : null}
-        {loading ? (
-          <Loading />
-        ) : (
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center"
-            }}
-          >
-            <DIV>
-              {contacts.length === 0 ? (
-                <h1 style={{ color: "white" }}>Empty Contacts</h1>
-              ) : null}
-              {contacts.map((contact) => {
-                return (
-                  <PlaceHolder
-                    onClick={() => handleDetails(contact.id)}
-                    key={contact.id}
-                  >
-                    <Icon className="fas fa-user"></Icon>
-                    {contact.Name}
-                  </PlaceHolder>
-                );
-              })}
-              {/* <ButtonComponent type="primary">
+	useEffect(
+		() => {
+			dispatch(contactListAction());
+		},
+		[ dispatch, adContact, fav, edited ]
+	);
+
+	const handleFav = (id) => {
+		if (fav === false) {
+			setFav(true);
+		}
+		else if (fav === true) {
+			setFav(false);
+		}
+		dispatch(addToFavouriteAction(id, fav));
+		setID(id);
+	};
+	const handleEdit = (id) => {
+		history.push(`/details/${id}`);
+	};
+	return (
+		<div className='App'>
+			<DIVWrapper>
+				{error ? <Alert type='error' message='Something went wrong!' /> : null}
+				{loading ? (
+					<Loading />
+				) : (
+					<div
+						style={{
+							display: 'flex',
+							justifyContent: 'center',
+							alignItems: 'center',
+						}}
+					>
+						<DIV>
+							{contacts.length === 0 ? <h1 style={{ color: 'white' }}>Empty Contacts</h1> : null}
+							{contacts.map((contact) => {
+								return (
+									<PlaceHolder key={contact.Name}>
+										<div>
+											<div>
+												<Icon className='fas fa-user' />
+												{contact.Name}
+											</div>
+										</div>
+										<div>
+											{contact.Favourite === true ? (
+												<Icon
+													onClick={() => handleFav(contact.id)}
+													className='fas fa-heart'
+													style={{ color: 'white' }}
+												/>
+											) : (
+												<Icon onClick={() => handleFav(contact.id)} className='far fa-heart' />
+											)}
+
+											<Icon onClick={() => handleEdit(contact.id)} className='far fa-edit' />
+										</div>
+									</PlaceHolder>
+								);
+							})}
+							{/* <ButtonComponent type="primary">
               <Icon className="far fa-edit" />
             </ButtonComponent>
             <ButtonComponent type="danger">
               <Icon className="fas fa-trash" />
             </ButtonComponent> */}
-            </DIV>
-          </div>
-        )}
-      </DIVWrapper>
-    </div>
-  );
+						</DIV>
+					</div>
+				)}
+			</DIVWrapper>
+		</div>
+	);
 }
